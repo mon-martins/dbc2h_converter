8556ba99ed19b6c2caad36c4b43d4114df9e19f4
Ramon Martins <ramon.martins@uem.com.br>
2024-03-20 13:55:39 -0300

    V1.0.0 ===============================================
    - Added float transmission
    - Added named values
    - Added macro CAN_GET_VALUE_BY_NAME
    
    ===============================================
    
    -

--- dbc2h_converter.py
+++ dbc2h_converter.py
@@ -29,12 +29,34 @@ header.write( "/************************************************************/\n"
 header.write( "//====================Messages Proprieties====================\n")
 header.write( "/************************************************************/\n")
 header.write( "\n")
+
+mask_ones = 0x00000000
+mask_zeros= 0x00000000
+
+for message in dbc_inst.messages:
+    if system_name in message.receivers:
+        mask_ones  |= message.frame_id
+        mask_zeros |= ~message.frame_id
+
+mask_ones  &= 0x00FFFF00
+mask_zeros &= 0x00FFFF00
+
+receive_id   = f"0x{mask_ones & 0x1FFFFFFF:08x}"
+receive_mask = f"0x{(~(mask_ones^mask_zeros)&0x1FFFFFFF):08x}"
+
+header.write( "\n")
+header.write( "\n")
+header.write( f"#define CAN_MSG_RECEIVE_ID {receive_id}\n")
+header.write( f"#define CAN_MSG_RECEIVE_MASK {receive_mask}\n")
+header.write( "\n")
+header.write( "\n")
+
 for message in dbc_inst.messages:
     header.write( "\n")
     header.write( "/************************************************************/\n")
     header.write(f"// Message: {message.name}\n")
     header.write( "/************************************************************/\n")
-    header.write(f"#define CAN_MSG_{message.name}\n")
+    header.write(f"#define CAN_MSG_{message.name} CAN_MSG_{message.name}\n")
     header.write(f"#define CAN_MSG_{message.name}_FRAME_ID {hex(message.frame_id)}\n")
     header.write( "\n")
     header.write( "typedef struct{\n")
@@ -48,13 +70,27 @@ for message in dbc_inst.messages:
             header.write(f"    uint64_t padding_{bit_start}:{padding_bits};\n")
         bit_start = bit_start+padding_bits
 
-        if signal.is_signed:
-            sig_type = 'uint64_t'
-        else:
+        if signal.is_float:
+            if signal.length == 32:
+                sig_type = 'float32_t'
+                if not(bit_start == 0 or bit_start == 32):
+                    raise Exception("the float32 value must be init on bit 0 or bit 32")
+            elif signal.length ==64:
+                sig_type = 'float64_t'
+                if not(bit_start == 0):
+                    raise Exception("the float64 value must be init on bit 0")
+            else:
+                raise Exception("A float pointer must be 32bit or 64bit sized")
+        elif signal.is_signed:
             sig_type = 'int64_t'
+        else:
+            sig_type = 'uint64_t'
         
         header.write(f"    // CAN_SIG_{signal.name} , bits {bit_start}-{bit_start+signal.length-1}\n")
-        header.write(f"    {sig_type} CAN_SIG_{signal.name}:{signal.length};\n")
+        header.write(f"    {sig_type} CAN_SIG_{signal.name}")
+        if(not signal.is_float):
+            header.write(f":{signal.length}")
+        header.write( ";\n")
         bit_start = bit_start + signal.length
     if bit_start<64:
         header.write(f"    // undef {bit_start}-{63} \n")
@@ -65,7 +101,7 @@ for message in dbc_inst.messages:
     header.write("\n")
     for signal in message.signals:
         header.write(f"// Signal: {signal.name}\n")
-        header.write(f"#define CAN_SIG_{signal.name}\n")
+        header.write(f"#define CAN_SIG_{signal.name} CAN_SIG_{signal.name}\n")
         if signal.is_signed:
             header.write(f"#define CAN_MSG_{message.name}_CAN_SIG_{signal.name}_TRANSMISSION_TYPE int64_t\n")
         else:
@@ -98,7 +134,10 @@ for message in dbc_inst.messages:
         total_size = ( (2**(signal.length)-1) )
         max_encoded = (signal.maximum - signal.offset)/signal.scale
         min_encoded = (signal.minimum - signal.offset)/signal.scale
-        if signal.is_signed:
+        
+        if signal.is_float:
+            pass
+        elif signal.is_signed:
             negative_size = -(2**(signal.length-1)-1)
             if( max_encoded > total_size + negative_size):
                 header.write( "#error \"the maximum must fits the space alocated\"\n")
@@ -113,28 +152,16 @@ for message in dbc_inst.messages:
             if(  min_encoded < 0  ):
                 header.write( "#error \"the minimum must be equals or larger than 0\"\n")
                 raise Exception(f"the minimum of {signal.name} in {message.name} must be equals or larger than 0, the encoded value is {min_encoded}")
+
+        if signal.choices:
+            header.write(f"// Named Values to Signal: {signal.name}\n")
+            for named_value in signal.choices:
+                header.write(f"#define CAN_VALUE_{signal.choices[named_value]} CAN_VALUE_{signal.choices[named_value]}\n")
+                header.write(f"#define CAN_MSG_{message.name}_CAN_SIG_{signal.name}_CAN_VALUE_{signal.choices[named_value]} {named_value}\n")
+        
         header.write("\n")
 
-mask_ones = 0x00000000
-mask_zeros= 0x00000000
 
-for message in dbc_inst.messages:
-    if system_name in message.receivers:
-        mask_ones  |= message.frame_id
-        mask_zeros |= ~message.frame_id
-
-mask_ones  &= 0x00FFFF00
-mask_zeros &= 0x00FFFF00
-
-receive_id   = f"0x{mask_ones & 0x1FFFFFFF:08x}"
-receive_mask = f"0x{(~(mask_ones^mask_zeros)&0x1FFFFFFF):08x}"
-
-header.write( "\n")
-header.write( "\n")
-header.write( f"#define CAN_MSG_RECEIVE_ID {receive_id}\n")
-header.write( f"#define CAN_MSG_RECEIVE_MASK {receive_mask}\n")
-header.write( "\n")
-header.write( "\n")
 
 header.write( "\n")
 header.write( "\n")
@@ -162,4 +189,13 @@ header.write( "//arg _VALUE: the value to be encoded \n")
 header.write( "//return IS_VALID: boolean informing if this value is valid\n")
 header.write( "\n")
 header.write( "#define CAN_SIG_IS_VALID( _CAN_MSG , _CAN_SIG , _VALUE ) \\\n")
-header.write( "( _CAN_MSG##_##_CAN_SIG##_MINIMUM < _VALUE ) && ( _VALUE < _CAN_MSG##_##_CAN_SIG##_MAXIMUM)\n")
+header.write( "( _CAN_MSG##_##_CAN_SIG##_MINIMUM <= _VALUE ) && ( _VALUE <= _CAN_MSG##_##_CAN_SIG##_MAXIMUM)\n")
+header.write( "\n")
+header.write( "\n")
+header.write( "//arg _CAN_MSG: the name of the message with prefix CAN_MSG_ \n")
+header.write( "//arg _CAN_SIG: the name of the signal  with prefix CAN_SIG_ \n")
+header.write( "//arg _CAN_VALUE: the name of the value with prefix CAN_VALUE \n")
+header.write( "//return VALUE: value of the named value\n")
+header.write( "\n")
+header.write( "#define CAN_GET_VALUE_BY_NAME( _CAN_MSG , _CAN_SIG , _CAN_VALUE ) \\\n")
+header.write( "( _CAN_MSG##_##_CAN_SIG##_##_CAN_VALUE )\n")
